d0460dba01915309cecdcf22e30b02942637f4d3
Arwindo <arwindoid@gmail.com>
2024-11-08 15:11:55 +0700

    Create: Initialize controller and define routing

---
+++ app/Http/Controllers/zonaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class zonaController extends Controller
+{
+    public function zona1()
+    {
+        return view("Zona.zona1");
+    }
+
+    public function zona2()
+    {
+        return view("Zona.zona2");
+    }
+
+    public function zona3()
+    {
+        return view("Zona.zona3");
+    }
+
+    public function zona4()
+    {
+        return view("Zona.zona4");
+    }
+
+    public function zona5()
+    {
+        return view("Zona.zona5");
+    }
+
+    public function zona6()
+    {
+        return view("Zona.zona6");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\registrasiController;
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\zonaController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Autentikasi
+Route::get('/', [loginController::class, 'index'])->name('login');
+Route::get('/registrasi', [registrasiController::class, 'index'])->name('registrasi');
+Route::post('/logout', [loginController::class, 'index'])->name('logout');
+
+// Dahboard
+Route::get('/dashboard', [dashboardController::class, 'index'])->name('dashboard');
+
+// fitur  utama
+Route::prefix('zona')->name('zona.')->group(function () {
+    Route::get('/1', [ZonaController::class, 'zona1'])->name('1');
+    Route::get('/2', [ZonaController::class, 'zona2'])->name('2');
+    Route::get('/3', [ZonaController::class, 'zona3'])->name('3');
+    Route::get('/4', [ZonaController::class, 'zona4'])->name('4');
+    Route::get('/5', [ZonaController::class, 'zona5'])->name('5');
+    Route::get('/6', [ZonaController::class, 'zona6'])->name('6');
 });
